e2940af607ee0f4db161360fb878134b642a8325
Emmanuel Cousin <emmanuel@hey.com>
2025-10-08 16:47:31 -0300

    Update merchant_accounts_controller_spec.rb

--- spec/controllers/admin/merchant_accounts_controller_spec.rb
+++ spec/controllers/admin/merchant_accounts_controller_spec.rb
@@ -15,35 +15,74 @@ describe Admin::MerchantAccountsController do
   end
 
   describe "GET show" do
-    let(:merchant_account) { MerchantAccount.gumroad(StripeChargeProcessor.charge_processor_id) }
-
-    it "renders the page successfully" do
-      get :show, params: { id: merchant_account }
-
-      expect(response).to be_successful
-      expect(response).to render_template(:show)
-    end
-
     context "for merchant accounts of type paypal", :vcr do
+      let(:merchant_account) { create :merchant_account_paypal, charge_processor_merchant_id: "B66YJBBNCRW6L" }
+
       it "returns the email address associated with the paypal account" do
-        paypal_merchant_account = create(:merchant_account_paypal, charge_processor_merchant_id: "B66YJBBNCRW6L")
+        get :show, params: { id: merchant_account.id }
 
-        get :show, params: { id: paypal_merchant_account.id }
+        expect(response.body).to include("data-page")
+        expect(response.body).to include("Admin/MerchantAccounts/Show")
 
-        expect(response.body).to have_content("Email \"sb-byx2u2205460@business.example.com\"", normalize_ws: true)
+        data_page = response.body.match(/data-page="([^"]+)"/)[1]
+        json_object = JSON.parse(CGI.unescapeHTML(data_page))
+        props = json_object["props"]
+
+        expect(props["live_attributes"]).to eq({
+          "Email" => "sb-byx2u2205460@business.example.com"
+        })
       end
     end
 
     context "for merchant accounts of type stripe", :vcr do
+      let(:merchant_account) { create :merchant_account, charge_processor_merchant_id: "acct_19paZxAQqMpdRp2I" }
+
       it "returns the charges and payouts related flags" do
-        stripe_merchant_account = create(:merchant_account, charge_processor_merchant_id: "acct_19paZxAQqMpdRp2I")
+        get :show, params: { id: merchant_account.id }
 
-        get :show, params: { id: stripe_merchant_account.id }
+        expect(response.body).to include("data-page")
+        expect(response.body).to include("Admin/MerchantAccounts/Show")
 
-        expect(response.body).to have_content("Charges enabled false", normalize_ws: true)
-        expect(response.body).to have_content("Payout enabled false", normalize_ws: true)
-        expect(response.body).to have_content("Disabled reason \"rejected.fraud\"", normalize_ws: true)
-        expect(response.body).to have_content("Fields needed", normalize_ws: true)
+        data_page = response.body.match(/data-page="([^"]+)"/)[1]
+        json_object = JSON.parse(CGI.unescapeHTML(data_page))
+        props = json_object["props"]
+
+        expect(props["live_attributes"]).to eq({
+          "Charges enabled" => false,
+          "Payout enabled" => false,
+          "Disabled reason" => "rejected.fraud",
+          "Fields needed" => {
+            "alternatives" => [],
+            "current_deadline" => nil,
+            "currently_due" => [
+              "individual.address.city",
+              "individual.address.line1",
+              "individual.address.postal_code",
+              "individual.address.state",
+              "individual.id_number",
+              "individual.nationality"
+            ],
+            "disabled_reason" => "rejected.fraud",
+            "errors" => [],
+            "eventually_due" => [
+              "individual.address.city",
+              "individual.address.line1",
+              "individual.address.postal_code",
+              "individual.address.state",
+              "individual.id_number",
+              "individual.nationality"
+            ],
+            "past_due" => [
+              "individual.address.city",
+              "individual.address.line1",
+              "individual.address.postal_code",
+              "individual.address.state",
+              "individual.id_number",
+              "individual.nationality"
+            ],
+            "pending_verification" => []
+          }
+        })
       end
     end
   end
