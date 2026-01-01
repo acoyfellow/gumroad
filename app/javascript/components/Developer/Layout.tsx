c320696110899f6d871306036e0e784e4efd6cb7
Romit <romitgabani@icloud.com>
2026-01-01 10:04:57 +0530

    update

--- app/javascript/components/Developer/Layout.tsx
+++ app/javascript/components/Developer/Layout.tsx
@@ -9,14 +9,14 @@ const pageNames = {
   ping: "Ping",
   api: "API",
 };
+export type Page = keyof typeof pageNames;
 
-export const Layout = ({
-  currentPage,
-  children,
-}: {
-  currentPage: keyof typeof pageNames;
-  children?: React.ReactNode;
-}) => (
+type Props = {
+  currentPage: Page;
+  children: React.ReactNode;
+};
+
+export const Layout = ({ currentPage, children }: Props) => (
   <div>
     <PageHeader title={pageNames[currentPage]}>
       <Tabs>
@@ -26,8 +26,8 @@ export const Layout = ({
         <Tab isSelected={currentPage === "ping"} asChild>
           <Link href={Routes.ping_path()}>Ping</Link>
         </Tab>
-        <Tab isSelected={currentPage === "api"} href={Routes.api_path()}>
-          API
+        <Tab isSelected={currentPage === "api"} asChild>
+          <Link href={Routes.api_path()}>API</Link>
         </Tab>
       </Tabs>
     </PageHeader>
