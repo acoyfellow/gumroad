5ae674689b7ef8002031e24ccad25d7377d0668b
Vedant Vaghasiya <vedantvaghasiya@Vedants-Mac-mini.local>
2026-01-29 17:41:32 +0530

    chore

--- app/javascript/pages/UrlRedirects/UnavailablePageLayout.tsx
+++ app/javascript/pages/UrlRedirects/UnavailablePageLayout.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import MetaTags from "$app/layouts/components/MetaTags";
+
 import { Layout, type LayoutProps } from "$app/components/server-components/DownloadPage/Layout";
 
 export type UnavailablePageProps = Omit<LayoutProps, "content_unavailability_reason_code" | "children">;
