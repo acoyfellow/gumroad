92bdb004dde0dc657de966425a48645c93bc2410
Vedant Vaghasiya <vedantvaghasiya@Vedants-Mac-mini.local>
2026-01-29 17:07:09 +0530

    update layout

--- app/javascript/pages/UrlRedirects/UnavailablePageLayout.tsx
+++ app/javascript/pages/UrlRedirects/UnavailablePageLayout.tsx
@@ -1,29 +1,22 @@
-import { Head } from "@inertiajs/react";
 import * as React from "react";
 
+import MetaTags from "$app/layouts/components/MetaTags";
 import { Layout, type LayoutProps } from "$app/components/server-components/DownloadPage/Layout";
 
 export type UnavailablePageProps = Omit<LayoutProps, "content_unavailability_reason_code" | "children">;
 
 export const UnavailablePageLayout = ({
-  titleSuffix,
   contentUnavailabilityReasonCode,
   children,
   ...layoutProps
 }: UnavailablePageProps & {
-  titleSuffix: string;
   contentUnavailabilityReasonCode: LayoutProps["content_unavailability_reason_code"];
   children: React.ReactNode;
-}) => {
-  const productName = layoutProps.purchase?.product_name ?? layoutProps.installment?.name ?? "";
-  const title = productName ? `${productName} - ${titleSuffix}` : titleSuffix;
-
-  return (
-    <>
-      <Head title={title} />
-      <Layout {...layoutProps} content_unavailability_reason_code={contentUnavailabilityReasonCode}>
-        {children}
-      </Layout>
-    </>
-  );
-};
+}) => (
+  <>
+    <MetaTags />
+    <Layout {...layoutProps} content_unavailability_reason_code={contentUnavailabilityReasonCode}>
+      {children}
+    </Layout>
+  </>
+);
