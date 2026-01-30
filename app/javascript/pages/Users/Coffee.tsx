b1fd5ce96dadc4269cc2cdb5127d4bcb67457c52
TenzDelek <tibetdelek@gmail.com>
2026-01-30 16:14:14 +0530

    unwanted prop

--- app/javascript/pages/Users/Coffee.tsx
+++ app/javascript/pages/Users/Coffee.tsx
@@ -4,15 +4,13 @@ import { cast } from "ts-safe-cast";
 
 import { CreatorProfile } from "$app/parsers/profile";
 
-import { Product, Purchase, WishlistForProduct } from "$app/components/Product";
+import { Product, Purchase } from "$app/components/Product";
 import { Layout as ProfileLayout } from "$app/components/Profile/Layout";
 import { CoffeeProduct } from "$app/components/Product/CoffeeProduct";
 
 type Props = {
   product: Product;
   purchase: Purchase | null;
-  wishlists: WishlistForProduct[];
-  discount_code: { valid: boolean; code?: string; discount?: unknown } | null;
   creator_profile: CreatorProfile;
   custom_styles: string;
 };
@@ -26,12 +24,7 @@ export default function CoffeePage() {
         <style>{custom_styles}</style>
       </Head>
       <ProfileLayout creatorProfile={creator_profile} hideFollowForm>
-        <CoffeeProduct
-          product={product}
-          purchase={purchase}
-          selection={null}
-          className="mx-auto w-full max-w-6xl lg:px-0"
-        />
+        <CoffeeProduct product={product} purchase={purchase} className="mx-auto w-full max-w-6xl lg:px-0" />
       </ProfileLayout>
     </>
   );
