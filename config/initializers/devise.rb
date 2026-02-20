d5d84fa40c7cb7e0cbb7d725eaa81c7898a072a3
Jordan Coeyman <coeyman@gmail.com>
2026-02-20 15:06:01 -0500

    Revert "fix(auth): use specific cookie domain on Railway to fix browser login"
    This reverts commit ea6ff963ecad15cc212595c5da0f6c821841c373.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -166,9 +166,7 @@ Devise.setup do |config|
 
   # Options to be passed to the created cookie. For instance, you can set
   # secure: true in order to force SSL only cookies.
-  if DOMAIN&.include?("railway.app")
-    config.rememberable_options = { domain: DOMAIN }
-  end
+  # config.rememberable_options = {}
 
   # ==> Configuration for :validatable
   # Range for password length.
